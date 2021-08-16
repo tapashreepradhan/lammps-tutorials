0f4d230a03752c22451602bc3ce1cb0a69db698d
Tapashree Pradhan <50409780+tapashreepradhan@users.noreply.github.com>
2021-08-16 12:46:40 +0530

    Add files via upload

---
+++ stress_strain_tutorial4.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 16 11:29:23 2021
+
+@author: tapashreepradhan
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+results = []
+with open('Al_comp_100.def1.txt',newline='') as file:
+    reader = csv.reader(file, delimiter=' ')
+    next(reader)  # Skip header row.
+    for row in reader:
+        row2 = [float(i) for i in row]
+        results.append(row2)
+        #print(row2)
+        
+results2 = -1 * np.transpose(results)
+plt.plot(results2[0],results2[1], '-or', label='Stress in X', lw=2, markersize = 1, mec = 'r', mfc = 'r')
+plt.plot(results2[0],results2[2], '-ob', label='Stress in Y', lw=2, markersize = 1, mec = 'b', mfc = 'b')
+plt.plot(results2[0],results2[3], '-og', label='Stress in Z', lw=2, markersize = 1, mec = 'g', mfc = 'g')
+plt.xlabel('Strain',fontsize=16)
+plt.ylabel('Stress (GPa)',fontsize=16)
+plt.title('Stress versus Strain',fontsize=16)
+plt.legend(fontsize=12)
+#plt.axis(aspect='equal')
+plt.ylim(0,10)
+plt.show()     
+
